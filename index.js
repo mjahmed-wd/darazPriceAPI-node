26dc3115d818b1f0a3cd689d0438aa51de95769b
Jubair <mjahmed.wd@gmail.com>
2022-02-19 01:03:27 +0600

    port

--- index.js
+++ index.js
@@ -36,4 +36,4 @@ app.get("/bd/:productId", (req, res) => {
     })();
 })
 
-app.listen(process.env.port || 5000, () => console.log("listening on port 5000"));
+app.listen(process.env.PORT || 5000);
