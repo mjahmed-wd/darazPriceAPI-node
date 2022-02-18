68bb79a054a1458aef595fe5e8af32f779cfc9b9
Jubair <mjahmed.wd@gmail.com>
2022-02-19 00:53:59 +0600

    ready to deploy

--- index.js
+++ index.js
@@ -1,45 +1,39 @@
 import superagent from 'superagent';
+import express from 'express';
+import cors from 'cors';
+const app = express();
+app.use(cors());
 
-(async () => {
-    try {
-        const res = await superagent.get('https://www.daraz.com.bd/products/6-tm-i164978002-s1097972599.html?spm=a2a0e.home.flashSale.3.735212f7Vhhb8E&search=1&mp=1&c=fs');
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
 
-        const matchingData = res?.text?.match("var pdpTrackingData = (\"{.*}\")")
-        const productStringData = (matchingData["0"].match("(\"{.*}\")")["0"]).replace(/\\/g, '');
-        const data = JSON.parse(productStringData.substring(1, productStringData.length - 1));
+app.get("/bd/:productId", (req, res) => {
+    const { productId } = req.params;
 
-        let product = {}
-        product['name'] = data['pdt_name']
-        product['seller'] = data['seller_name']
-        product['price'] = data['pdt_price'].split(' ')[1]
-        product['currencyCode'] = data['core']['currencyCode']
-        product['image'] = data['pdt_photo']
-        product['category'] = data['pdt_category']
-        product['discount'] = data['pdt_discount']
-        product['sku'] = data['pdt_simplesku']
-        product['countryCode'] = data['core']['country']
-        console.log(product);
-    } catch (err) {
-        console.log(err.message); //can be console.error
-    }
-})();
-// import { text } from "./htmlpage.js"
+    (async () => {
+        try {
+            const response = await superagent.get(`https://www.daraz.com.bd/products/${productId}`);
 
+            const matchingData = response?.text?.match("var pdpTrackingData = (\"{.*}\")")
+            const productStringData = (matchingData["0"].match("(\"{.*}\")")["0"]).replace(/\\/g, '');
+            const data = JSON.parse(productStringData.substring(1, productStringData.length - 1));
 
-// const matchingData = text.match("var pdpTrackingData = (\"{.*}\")")
-// const productStringData = (matchingData["0"].match("(\"{.*}\")")["0"])
-// const data = JSON.parse(productStringData.substring(1, productStringData.length - 1));
+            let product = {}
+            product['name'] = data['pdt_name']
+            product['seller'] = data['seller_name']
+            product['price'] = data['pdt_price'].split(' ')[1]
+            product['currencyCode'] = data['core']['currencyCode']
+            product['image'] = data['pdt_photo']
+            product['category'] = data['pdt_category']
+            product['discount'] = data['pdt_discount']
+            product['sku'] = data['pdt_simplesku']
+            product['countryCode'] = data['core']['country']
+            res.json(product);
+        } catch (err) {
+            console.log(err.message); //can be console.error
+        }
+    })();
+})
 
-// let product = {}
-// product['name'] = data['pdt_name']
-// product['seller'] = data['seller_name']
-// product['price'] = data['pdt_price'].split(' ')[1]
-// product['currencyCode'] = data['core']['currencyCode']
-// product['image'] = data['pdt_photo']
-// product['category'] = data['pdt_category']
-// product['discount'] = data['pdt_discount']
-// product['sku'] = data['pdt_simplesku']
-// product['countryCode'] = data['core']['country']
-// console.log(product);
-
-// console.log(productData)
+app.listen(process.env.port || 5000, () => console.log("listening on port 5000"));
