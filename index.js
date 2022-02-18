eef50e8ada7dca7aedc890928000d3ccbb703624
Jubair <mjahmed.wd@gmail.com>
2022-02-18 23:15:33 +0600

    json parsed data

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 import {text } from "./htmlpage.js"
 
 const matchingData = text.match("var pdpTrackingData = (\"{.*}\")")
+const productData = (matchingData["0"].match("(\"{.*}\")")["0"])
 
-console.log(typeof matchingData["0"])
+
+
+const replaced = JSON.parse(productData.substring(1, productData.length-1));
+console.log(replaced); // 👉️ test
+
+// console.log(productData)
