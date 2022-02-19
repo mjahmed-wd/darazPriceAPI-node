fd666018b5050efae30da0dac588d058082b1a31
Jubair <mjahmed.wd@gmail.com>
2022-02-19 12:40:48 +0600

    router added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const superagent = require('superagent');
 const express = require('express');
 const cors = require('cors');
+const productRouter = require("./router/productRouter.js");
 const cron = require('node-cron');
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
@@ -20,63 +21,37 @@ mongoose
 app.use(cors());
 app.use(express.json());
 
-cron.schedule('*/10 * * * * * ', () => {
-    ["pu-i190005767-s1137519941.html", "i199677125-s1146833359.html", "pu-i211024792-s1160899536.html", "bogesi-i215889192-s1164326843.html"].forEach(productId =>
-        (async () => {
-            try {
-                const response = await superagent.get(`https://www.daraz.com.bd/products/${productId}`);
+// cron.schedule('*/10 * * * * * ', () => {
+//     ["pu-i190005767-s1137519941.html", "i199677125-s1146833359.html", "pu-i211024792-s1160899536.html", "bogesi-i215889192-s1164326843.html"].forEach(productId =>
+//         (async () => {
+//             try {
+//                 const response = await superagent.get(`https://www.daraz.com.bd/products/${productId}`);
 
-                const matchingData = response?.text?.match("var pdpTrackingData = (\"{.*}\")")
-                const productStringData = (matchingData["0"].match("(\"{.*}\")")["0"]).replace(/\\/g, '');
-                const data = JSON.parse(productStringData.substring(1, productStringData.length - 1));
+//                 const matchingData = response?.text?.match("var pdpTrackingData = (\"{.*}\")")
+//                 const productStringData = (matchingData["0"].match("(\"{.*}\")")["0"]).replace(/\\/g, '');
+//                 const data = JSON.parse(productStringData.substring(1, productStringData.length - 1));
 
-                let product = {}
-                product['name'] = data['pdt_name']
-                product['seller'] = data['seller_name']
-                product['price'] = data['pdt_price'].split(' ')[1]
-                product['currencyCode'] = data['core']['currencyCode']
-                product['image'] = data['pdt_photo']
-                product['category'] = data['pdt_category']
-                product['discount'] = data['pdt_discount']
-                product['sku'] = data['pdt_simplesku']
-                product['countryCode'] = data['core']['country']
-               return console.log(product?.name, new Date());
-            } catch (err) {
-                console.log(err.message); //can be console.error
-            }
-        })())
-});
+//                 let product = {}
+//                 product['name'] = data['pdt_name']
+//                 product['seller'] = data['seller_name']
+//                 product['price'] = data['pdt_price'].split(' ')[1]
+//                 product['currencyCode'] = data['core']['currencyCode']
+//                 product['image'] = data['pdt_photo']
+//                 product['category'] = data['pdt_category']
+//                 product['discount'] = data['pdt_discount']
+//                 product['sku'] = data['pdt_simplesku']
+//                 product['countryCode'] = data['core']['country']
+//                return console.log(product?.name, new Date());
+//             } catch (err) {
+//                 console.log(err.message); //can be console.error
+//             }
+//         })())
+// });
 
-app.get("/", (req, res) => {
+app.get("/", (req, res) => { 
     res.send("Hello World!");
 });
 
-app.get("/bd/:productId", (req, res) => {
-    const { productId } = req.params;
-
-    (async () => {
-        try {
-            const response = await superagent.get(`https://www.daraz.com.bd/products/${productId}`);
-
-            const matchingData = response?.text?.match("var pdpTrackingData = (\"{.*}\")")
-            const productStringData = (matchingData["0"].match("(\"{.*}\")")["0"]).replace(/\\/g, '');
-            const data = JSON.parse(productStringData.substring(1, productStringData.length - 1));
-
-            let product = {}
-            product['name'] = data['pdt_name']
-            product['seller'] = data['seller_name']
-            product['price'] = data['pdt_price'].split(' ')[1]
-            product['currencyCode'] = data['core']['currencyCode']
-            product['image'] = data['pdt_photo']
-            product['category'] = data['pdt_category']
-            product['discount'] = data['pdt_discount']
-            product['sku'] = data['pdt_simplesku']
-            product['countryCode'] = data['core']['country']
-            res.json(product);
-        } catch (err) {
-            console.log(err.message); //can be console.error
-        }
-    })();
-})
+app.get("/bd/:productId", productRouter)
 
 app.listen(process.env.PORT || 5000);
