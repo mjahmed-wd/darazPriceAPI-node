18f7ee984ef58b4b599662636a1130c5abb8ea5b
Jubair <mjahmed.wd@gmail.com>
2022-02-19 23:54:30 +0600

    cron job moved to controller

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 const superagent = require('superagent');
 const express = require('express');
 const cors = require('cors');
-const productRouter = require("./router/productRouter.js");
-const cron = require('node-cron');
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
-const router = express.Router();
-const Product = require("./models/product.js");
-const CronItem = require("./models/cronItem.js");
-const res = require('express/lib/response');
-
 const app = express();
+const productRouter = require("./router/productRouter.js");
+const cronJob = require("./controller/cronJob");
 dotenv.config();
 
 // request parser
@@ -26,59 +21,8 @@ mongoose
     .then(() => console.log("connection successful!"))
     .catch((err) => console.log(err));
 
-
-const cronJob= () => cron.schedule('*/10 * * * * * ', () => {
-    let cronItems;
-    (async () => {
-        cronItems = await CronItem.find({}).select('url -_id').lean();
-        if (cronItems) {
-            console.log({ cronItems });
-            cronItems?.forEach(cronItem =>
-                (async () => {
-                    try {
-                        const response = await superagent.get(`https://www.daraz.com.bd/products/${cronItem.url}`);
-
-                        const productURL = `${(cronItem.url.match("(.*).html")[1])}.html`
-                        const matchingData = response?.text?.match("var pdpTrackingData = (\"{.*}\")")
-                        const productStringData = (matchingData["0"].match("(\"{.*}\")")["0"]).replace(/\\/g, '');
-                        const data = JSON.parse(productStringData.substring(1, productStringData.length - 1));
-
-                        let product = {}
-                        product['name'] = data['pdt_name']
-                        product['seller'] = data['seller_name']
-                        product['price'] = data['pdt_price'].split(' ')[1]
-                        product['currencyCode'] = data['core']['currencyCode']
-                        product['image'] = data['pdt_photo']
-                        product['category'] = data['pdt_category']
-                        product['discount'] = data['pdt_discount']
-                        product['sku'] = data['pdt_simplesku']
-                        product['countryCode'] = data['core']['country']
-                        product['url'] = productURL
-                        // return await console.log(product);
-                        try {
-                            // const newProduct = new Product(product);
-                            // const result = await newProduct.save();
-                            const result = await Product.findOneAndUpdate({ sku: product.sku }, { $push: { priceList: { date: new Date(), price: product.price } } }, (error, success) => {
-                                console.log(error ? error : success);
-                            });
-                            // res.status(200).json({
-                            //     message: result?.name + " added successfully!",
-                            // });
-                            console.log(result);
-                        } catch (err) {
-                            console.log("eror happend", err);
-                        }
-
-                    } catch (err) {
-                        console.log(err.message); //can be console.error
-                    }
-                })())
-        } else {
-            console.log("no result")
-        }
-    })()
-});
 cronJob();
+
 // const url = "https://www.daraz.com.bd/products/bogesi-i215889192-s1164326843.html"
 // console.log(url.match("products/(.*).html")[1])
 
